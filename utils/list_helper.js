77d56b9b99ea31961b08da5815e4b02932f8d072
gzavvo <gzavvo@gmail.com>
2021-02-24 13:15:24 +0200

    4.5: Helper functions and unit tests step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const minimized = (blog) => {
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  const mostLikedBlog =  blogs.find(blog => blog.likes === mostLikes)
+  return minimized(mostLikedBlog)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
