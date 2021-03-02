eeea66e7abf01cb7c1db13b613d6575b028a8829
gzavvo <gzavvo@gmail.com>
2021-03-02 11:10:46 +0200

    4.9: Blog list step2

--- tests/list_api.test.js
+++ tests/list_api.test.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../models/blog')
 const app = require('../app')
 const api = supertest(app)
+const helpers = require('./test_helpers')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helpers.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blogs have a unique identifier property named _id_', async () => {
+  const response = await helpers.blogsInDb()
+
+  response.forEach(blog => {
+    expect(blog.id).toBeDefined()
+  })
 })
 
 afterAll(() => {
