52058ffa12e50f5f678f2a2508ecda9054fd29e5
gzavvo <gzavvo@gmail.com>
2021-04-21 08:05:41 +0300

    5.22: bloglist end to end testing, step6

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
-const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
@@ -9,6 +8,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -29,6 +29,11 @@ app.use(middleware.userExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
